f1aa1dc817989a05addeaf6105aa8457beda3cbe
andreweryan <andrrvt15@gmail.com>
2024-01-13 16:31:59 -0500

    rewriting in polars

--- processor.py
+++ processor.py
@@ -1,8 +1,12 @@
 import os
+from pathlib import Path
+import numpy as np
+import polars as pl
 import pandas as pd
+from datetime import datetime
 from tqdm import tqdm
 import geopandas as gpd
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import warnings
 
@@ -11,33 +15,50 @@ warnings.filterwarnings("ignore")
 
 # data dowload: https://download.microsoft.com/download/F/4/8/F4894AA5-FDBC-481E-9285-D5F8C4C4F039/Geolife%20Trajectories%201.3.zip
 
+start = datetime.now()
 
-def process_data(user_id, path, df_list):
-    df = pd.read_csv(
+
+def extract_user_id(data_path):
+    return int(Path(data_path).parts[-3])
+
+
+def process_data(path, df_list):
+    df = pl.read_csv(
         path,
-        skiprows=6,
-        names=[
-            "latitude",
-            "longitude",
-            "zero",
-            "altitude",
-            "days_since",
-            "date_str",
-            "time_str",
-        ],
+        skip_rows=6,
+        columns=[0, 1, 3, 5, 6],
+        new_columns=["latitude", "longitude", "altitude", "date_str", "time_str"],
+        dtypes=[pl.Float64, pl.Float64, pl.Float64, pl.String, pl.String],
     )
-    df = df[df["latitude"].between(-90, 90) & df["longitude"].between(-180, 180)]
-    df.drop(["zero", "days_since"], axis=1, inplace=True)
-    df["timestamp"] = pd.to_datetime(df["date_str"] + " " + df["time_str"])
-    df["user_id"] = user_id
-    # data is recorded every ~1-5 seconds. Reduce to every minute
-    df_resampled = df.resample("1T", on="timestamp").first().reset_index(drop=True)
-    gdf = gpd.GeoDataFrame(
-        df_resampled,
-        geometry=gpd.points_from_xy(df_resampled.longitude, df_resampled.latitude),
-        crs="EPSG:4326",
+
+    df = df.with_columns(
+        pl.concat_str(
+            [pl.col("date_str"), pl.col("time_str")],
+            separator=" ",
+        ).alias("timestamp_str")
     )
-    df_list.append(gdf)
+
+    df = df.with_columns(
+        pl.col("timestamp_str").str.to_datetime().cast(pl.Datetime).alias("timestamp")
+    )
+    # data is recorded every ~1-5 seconds. Reduce/downsample to every 10s
+
+    df = (
+        df.set_sorted("timestamp")
+        .group_by_dynamic("timestamp", every="10s")
+        .agg(pl.col(pl.Float64).mean())
+    )
+
+    user_id = extract_user_id(path)
+
+    df = df.with_columns(pl.lit(user_id).alias("user_id"))
+
+    df.drop(["date_str", "time_str"])
+
+    df_list.append(df)
+
+    df = None
+
     return None
 
 
@@ -50,36 +71,31 @@ files = [
     if filename.endswith(".plt")
 ]
 
-file_map = list()
-
 df_list = list()
 
+# with tqdm(total=len(files), desc="Processing Data", unit="file") as progress_bar:
+#     for file in files:
+#         process_data(file, df_list)
+#         progress_bar.update(1)
+
 with tqdm(total=len(files), desc="Processing Data", unit="file") as progress_bar:
-    for file in files:
-        path, folder = os.path.split(os.path.dirname(file))
-        p, user_id = os.path.split(path)
-        process_data(user_id, file, df_list)
-        progress_bar.update(1)
+    with ThreadPoolExecutor(max_workers=None) as executor:
+        futures = {executor.submit(process_data, file, df_list): file for file in files}
+        for _ in as_completed(futures):
+            progress_bar.update(1)
 
-data = pd.concat(df_list, axis=0, ignore_index=True)
-print(data.head())
-print(f"Number of records in the dataset: {len(data)}")
-
-# for file in files:
-#     path, folder = os.path.split(os.path.dirname(file))
-#     p, user_id = os.path.split(path)
-#     file_map.append([user_id, file])
-
-# with tqdm(total=len(file_map), desc="Processing Data", unit="file") as progress_bar:
-#     with ThreadPoolExecutor(max_workers=None) as executor:
-#         futures = {executor.submit(process_data, file[0], file[1], df_list): file for file in file_map}
-#         for _ in as_completed(futures):
-#             progress_bar.update(1)
-
-gdf = gpd.GeoDataFrame(pd.concat(df_list, axis=0, ignore_index=True))
-print(gdf.head())
+pl_df = pl.concat(df_list)
+gdf = gpd.GeoDataFrame(
+    pl_df.to_pandas(),
+    geometry=gpd.points_from_xy(pl_df["longitude"], pl_df["latitude"]),
+    crs="EPSG:4326",
+)
 print(f"Number of records in the dataset: {len(gdf)}")
+print(gdf.head())
 
 out_path = os.path.join(os.path.dirname(data_dir), "geolife_points.parquet")
 
 gdf.to_parquet(out_path)
+
+end = datetime.now()
+print(f"Total processing time: {end - start}")
